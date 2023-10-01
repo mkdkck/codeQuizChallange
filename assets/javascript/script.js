add009057b23b701f5cc8f4512aae0e0f950e0fe
mkdkck <colin173@gmail.com>
2023-10-02 00:30:39 +1030

    basic HTML & CSS built

---
+++ assets/javascript/script.js
@@ -0,0 +1,5 @@
+var viewScores = document.querySelector(".viewScores");
+var timer = document.querySelector(".timer")
+var questions = document.querySelector(".questions")
+var choices = document.querySelector(".choices")
+var startBtn = document.querySelector(".startBtn")
