a7b4d1d71c6d15d2a6bf9d2abab5164df90df956
mkdkck <colin173@gmail.com>
2023-10-05 00:20:28 +1030

    modified the sturcture, main content finished

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,47 +1,49 @@
 var viewScores = document.querySelector(".viewScores");
-var timer = document.querySelector(".timer")
-var questions = document.querySelector(".questions")
-var choices = document.querySelector(".choices")
-var startBtn = document.querySelector(".startBtn")
-var notes = document.querySelector(".notes")
-var timeRemaining = localStorage.getItem("timeRemaining")
+var timer = document.querySelector(".timer");
+var questions = document.querySelector(".questions");
+var choices = document.querySelector(".choices");
+var startBtn = document.querySelector(".startBtn");
+var notes = document.querySelector(".notes");
+
+var timeRemaining = 60;
+var questionNum = 0;
+
+
 
 // questions and answers sets
-var q1 = {
+var questionLoop = [{
     content: "Commonly used data types DO NOT include:",
     options: ["strings", "booleans", "alerts", "numbers"],
-    correctAnswer:"3. alerts",
-}
-var q2 = {
+    correctAnswer:"alerts",
+},{
     content: "The condition in an if/else statement is enclosed with _______",
     options: ["quotes", "curly brackets", "parenthesis", "square brackets"],
-    correctAnswer:"2. curly brackets",
-}
-var q3 = {
+    correctAnswer:"curly brackets",
+},{
     content: "Which is the following methods is used to store an object to the local storage?",
     options: ["JSON.stringify()", "JONSON.stringify()", "JSON.spotify()", "JSON.parse()"],
-    correctAnswer:"1. JSON.stringify()",
-}
-var q4 = {
+    correctAnswer:"JSON.stringify()",
+},{
     content: "The document method: querySelector() is to return ______ Element(s) that matches the specified selector, or group of selectors.",
     options: ["the first", "all", "the last", "the second"],
-    correctAnswer:"1. the first",
-}
-var q5 = {
+    correctAnswer:"the first",
+},{
     content: "which of the followings is a class selector to set the class attribute in CSS",
     options: [".class{}", "#class{}", "var class = ''", "function(class) = {}"],
-    correctAnswer:"1. .class{}",
-}
-var questionLoop = [q1,q2,q3,q4,q5]
+    correctAnswer:".class{}",
+}]
 
 
 // startButton event
 startBtn.addEventListener("click", function(){
     timerStart()
-    var questionNum = 0
-    localStorage.setItem ("questionNum", questionNum)
-    var timeRemaining = 60
-    localStorage.setItem ("timeRemaining", timeRemaining)
+    choices.removeAttribute("hidden");
+
+    for (var i=0; i < 4; i++) {
+        var emptyBtn = document.createElement ("button");
+        emptyBtn.setAttribute("value", "");
+        choices.appendChild (emptyBtn);
+    };
 
     startBtn.setAttribute("hidden","true");
     notes.textContent = "";
@@ -51,28 +53,33 @@ startBtn.addEventListener("click", function(){
 
 // quetsion loop function starts here, achieving auto switching to the next questions when condition meets
 function moreQuestions() {
-    var questionNum = localStorage.getItem("questionNum")
-
+    questions.textContent = questionLoop[questionNum].content;
     for (var i=0; i < questionLoop[questionNum].options.length; i++) {
-        questions.textContent = questionLoop[questionNum].content;
-        var aLists = document.createElement ("button");
-        aLists.textContent= i+1 +". " + questionLoop[questionNum].options[i];
-        choices.appendChild (aLists);
+        choices.children[i].value = questionLoop[questionNum].options[i];
+        choices.children[i].textContent= i+1 +". " + choices.children[i].value;
     };
+    choices.addEventListener("click", pickAnswer);
+}
 
-    choices.addEventListener("click", function(event){
-        var chosen= event.target;
-        
-        if (chosen.innerHTML == questionLoop[questionNum].correctAnswer) {
-            notes.textContent="Correct!";
+function pickAnswer (event){
+    var chosen= event.target;
+   
+    if (chosen.value === questionLoop[questionNum].correctAnswer) {
+        notes.textContent="Your answer is CORRECT!";
+        setTimeout(function(){
             questionNum++;
-            localStorage.setItem("questionNum", questionNum);
-            moreQuestions();
-        } 
-    })
-
-    }
+            notes.textContent = "";
+            choices.removeEventListener("click", pickAnswer);
+            moreQuestions(); 
+        },1000);
+    } else {
+        timeRemaining = timeRemaining-15;
+        notes.textContent="Your answer is WRONG!";
+        pickAnswer();
+    }    
     
+}
+
 
 
 // Timer function
@@ -81,20 +88,17 @@ function timerStart(){
         timeRemaining--;
         timer.textContent = timeRemaining;
 
-        if (timeRemaining === 0){
+        if (timeRemaining < 0 || timeRemaining ===0){
             clearInterval(counting);
             gameOver();
         }
-
-    },1000)
+        
+    },1000);
 }
 
-// function gameOver(){
-
-// }
+function gameOver(){
+    questions.textContent = "Time is up, try harder next time!";
+    notes.textContent = "Please reload the page to restart.";
+    choices.setAttribute("hidden","true");
+}
 // console.log (questionNum);
-
-
-
-
-
