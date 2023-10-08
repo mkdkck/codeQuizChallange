33a2a7ba3e3db56a491aa59652108195681ed1b0
mkdkck <colin173@gmail.com>
2023-10-09 01:25:03 +1030

    finalised the whole fucntion, tested and debugged

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,9 +4,20 @@ var questions = document.querySelector(".questions");
 var choices = document.querySelector(".choices");
 var startBtn = document.querySelector(".startBtn");
 var notes = document.querySelector(".notes");
+var main = document.querySelector("#main");
+var endPage = document.querySelector("#end-screen");
+var score = document.querySelector("#final-score");
+var submit = document.querySelector("#submit");
+var input = document.querySelector("#initials");
+var form = document.querySelector("#scoreForm");
+var savedScore = [];
 
 var timeRemaining = 60;
 var questionNum = 0;
+var counting;
+var initials ={
+    int: "",
+    sec: "",};
 
 
 
@@ -34,71 +45,99 @@ var questionLoop = [{
 }]
 
 
-// startButton event
+// startButton event, create four blank buttons, hide start button, clear the notes section, starts the questions function.
 startBtn.addEventListener("click", function(){
     timerStart()
-    choices.removeAttribute("hidden");
 
     for (var i=0; i < 4; i++) {
         var emptyBtn = document.createElement ("button");
+        emptyBtn.setAttribute("class","btn")
         emptyBtn.setAttribute("value", "");
         choices.appendChild (emptyBtn);
     };
 
-    startBtn.setAttribute("hidden","true");
+    startBtn.setAttribute("class","hide");
     notes.textContent = "";
 
-    moreQuestions()
-})
+    moreQuestions();
+
+});
 
 // quetsion loop function starts here, achieving auto switching to the next questions when condition meets
 function moreQuestions() {
     questions.textContent = questionLoop[questionNum].content;
+
     for (var i=0; i < questionLoop[questionNum].options.length; i++) {
         choices.children[i].value = questionLoop[questionNum].options[i];
         choices.children[i].textContent= i+1 +". " + choices.children[i].value;
     };
-    choices.addEventListener("click", pickAnswer);
+    
+    pickAnswers();
 }
 
-function pickAnswer (event){
-    var chosen= event.target;
-   
-    if (chosen.value === questionLoop[questionNum].correctAnswer) {
-        notes.textContent="Your answer is CORRECT!";
+// eventlistener linstening to the click event on buttons to see if the user choose the right or wrong answers.
+function pickAnswers(){
+    choices.addEventListener("click", function click(event){
+        var chosen= event.target;
+
+        if (chosen.value === questionLoop[questionNum].correctAnswer) {
+            notes.textContent="Your answer is CORRECT!";
+        } else {
+            timeRemaining = timeRemaining-15;
+            notes.textContent="Your answer is WRONG!";
+        }
         setTimeout(function(){
             questionNum++;
             notes.textContent = "";
-            choices.removeEventListener("click", pickAnswer);
-            moreQuestions(); 
-        },1000);
-    } else {
-        timeRemaining = timeRemaining-15;
-        notes.textContent="Your answer is WRONG!";
-        pickAnswer();
-    }    
-    
+            choices.removeEventListener("click",click);
+            if (questionNum === questionLoop.length) {
+                scoreBoard();
+            }else {
+                moreQuestions();
+            };
+        },500);
+    });
 }
-
-
 
 // Timer function
 function timerStart(){
-    var counting = setInterval(function(){
+    counting = setInterval(function(){
         timeRemaining--;
         timer.textContent = timeRemaining;
 
-        if (timeRemaining < 0 || timeRemaining ===0){
+        if (timeRemaining < 0 || timeRemaining === 0){
             clearInterval(counting);
-            gameOver();
+            scoreBoard();
         }
-        
     },1000);
 }
 
-function gameOver(){
-    questions.textContent = "Time is up, try harder next time!";
-    notes.textContent = "Please reload the page to restart.";
-    choices.setAttribute("hidden","true");
+function scoreBoard(){
+    clearInterval(counting);
+    timer.textContent = timeRemaining;
+    main.setAttribute("class","hide");
+    endPage.removeAttribute("class","hide");
+    if (timeRemaining <= 0) {
+        timer.textContent = 0;
+        score.textContent = 0;
+    } else {
+        score.textContent = timeRemaining;
+    }    
 }
-// console.log (questionNum);
+
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+    if (input.value === "") {
+        window.alert ("Please type in your initials.");
+    } else {
+        saveScore();
+    }
+})
+
+function saveScore() {
+    initials.int = input.value;
+    initials.sec = score.innerHTML;
+    localStorage.setItem("newScore", JSON.stringify(initials));
+    window.location.replace("highscores.html")
+}
+
