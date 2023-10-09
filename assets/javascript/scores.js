8d395f2f3d8ed0d7d2ed531973422f0661c7dc43
mkdkck <colin173@gmail.com>
2023-10-09 22:13:34 +1030

    create readme & notes in the codes & fix small bugs

--- assets/javascript/scores.js
+++ assets/javascript/scores.js
@@ -3,27 +3,31 @@ var highScores=JSON.parse(localStorage.getItem("highScore")) || [];
 var hsList = document.querySelector("#highscores");
 var newScores = JSON.parse(localStorage.getItem("newScore")); 
 
+//initialize the high score array, prevent pushs null value from the newScore into the highScores array.
 if (highScores === null) {
   highScores = newScores;
 } else if (newScores === null){
   } else {
   highScores.push(newScores);
 };
-localStorage.setItem ("highScore",JSON.stringify(highScores));
 
+// set new records into the highScore, and remove newScore value to prevent refreshing page adding the same value back to the highScore array.
+localStorage.setItem ("highScore",JSON.stringify(highScores));
+localStorage.removeItem("newScore");
+
+//prevent the error message when there is no value in highScores array.
+// sort highscores by score property in descending order
 if (highScores === undefined) {
   } else {
     highScores.sort((a,b) => b.sec - a.sec);
 
 function printHighscores() {
-// sort highscores by score property in descending order
   for (var i = 0; i < highScores.length; i += 1) {
   var list = document.createElement("li");
   list.innerHTML = highScores[i].int +"---"+ highScores[i].sec + " Points";
   hsList.appendChild (list);
   } 
-};  
-localStorage.removeItem("newScore");
+};
 }
 
 // clear the whole list and clear the localStorage
